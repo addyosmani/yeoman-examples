7a46a4dc6d4ac7a33947d1800457cff571519435
pearlchen@google.com <pearlchen@google.com>
2014-01-17 14:40:07 -0800

    Minor whitespace formatting fixes so it looks good when file is viewed in Github.
    Matches the indentation of the codelab now too.

--- angular-localStorage-todos/app/scripts/controllers/main.js
+++ angular-localStorage-todos/app/scripts/controllers/main.js
@@ -1,26 +1,27 @@
 'use strict';
 
 angular.module('mytodoApp')
-    .controller('MainCtrl', function ($scope /*, localStorageService*/) {
+  .controller('MainCtrl', function ($scope /*, localStorageService*/) {
 
-/*
-        var todosInStore = localStorageService.get('todos');
+    /*
+    var todosInStore = localStorageService.get('todos');
 
-        $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
 
-        $scope.$watch('todos', function () {
-            localStorageService.add('todos', $scope.todos.join('\n'));
-          }, true);
- */
+    $scope.$watch('todos', function () {
+      localStorageService.add('todos', $scope.todos.join('\n'));
+    }, true);
+    */
 
-		$scope.todos = [];
+    $scope.todos = [];
 
-		$scope.addTodo = function () {
-            $scope.todos.push($scope.todo);
-            $scope.todo = '';
-          };
+    $scope.addTodo = function () {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+    };
 
-		$scope.removeTodo = function (index) {
-            $scope.todos.splice(index, 1);
-          };
-	});
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+    
+  });
