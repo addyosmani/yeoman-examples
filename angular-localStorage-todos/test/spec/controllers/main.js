5305f61fdeb8bff2dab86f2fc5b1b2bb8ba3b86c
unknown <478528@PC215054.cts.com>
2015-07-08 11:52:48 +0530

    Changed the test description

--- angular-localStorage-todos/test/spec/controllers/main.js
+++ angular-localStorage-todos/test/spec/controllers/main.js
@@ -26,7 +26,7 @@ describe('Controller: MainCtrl', function () {
       expect(scope.todos.length).toBe(1);
     });
 
-  it('should add items to the list', function () {
+  it('should add then remove an item from the list', function () {
       scope.todo = 'Test 1';
       scope.addTodo();
       scope.removeTodo(0);
